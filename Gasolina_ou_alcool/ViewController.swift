89140edab8b6db1764fdf9b97b7684893d8b33cf
marcelo.okasaki <marcelo.okasaki@gmail.com>
2022-08-17 20:05:57 -0300

    Initial Commit

---
+++ Gasolina_ou_alcool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gasolina_ou_alcool
+//
+//  Created by Marcelo on 17/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
